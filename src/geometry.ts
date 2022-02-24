f3759b2ca14f5fc525de6fe4a088364b67c8b240
Dimitre Ortt <dimiortt@gmail.com>
2022-02-24 04:00:49 -0300

    Created Line class

--- src/geometry.ts
+++ src/geometry.ts
@@ -1,15 +1,26 @@
-abstract class Shape {
-  abstract type: string;
-}
+type Shape = Point | Square | Circle | Rectangle;
 
-class Point implements Shape {
+class Point {
   type = 'Point';
-
   constructor(readonly x: number, readonly y: number) {}
 }
 
-class Square implements Shape {
+class Square {
   type = 'Square';
-
   constructor(readonly start: Point, readonly side: number) {}
+}
+
+class Circle {
+  type = 'Circle';
+  constructor(readonly center: Point, readonly radius: number) {}
+}
+
+class Rectangle {
+  type = 'Rectangle';
+  constructor(readonly start: Point, readonly finish: Point) {}
+}
+
+class Line {
+  type = 'Line';
+  constructor(readonly m: number, readonly c: number) {}
 }
