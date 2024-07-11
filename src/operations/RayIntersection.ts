649e45be63cc646e5c59380b02a2a59fba9dd9dc
Dimitre Ortt <dortt@symplicity.com>
2024-07-11 16:51:39 -0300

    remove extra space

--- src/operations/RayIntersection.ts
+++ src/operations/RayIntersection.ts
@@ -2,6 +2,7 @@ class RayIntersection {
     reflexion: Vector
 
     constructor(readonly originalRay: Ray, readonly intersection: Point, readonly normal: Vector) {
+        
         this.reflexion = originalRay.direction.reflect(normal)
     }
 }
